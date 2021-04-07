76ae63b68b69d088381286e21edd916096c50feb
Will <will@glozer.net>
2021-04-07 15:39:32 +0900

    extract netdiag crate

--- netdiag/src/icmp/mod.rs
+++
@@ -1,7 +0,0 @@
-pub use icmp4::IcmpV4Packet;
-pub use icmp6::IcmpV6Packet;
-
-pub mod icmp4;
-pub mod icmp6;
-
-mod echo;
--- netdiag/src/ping/mod.rs
+++
@@ -1,9 +0,0 @@
-pub use ping::Ping;
-pub use ping::Pinger;
-pub use probe::Probe;
-
-mod ping;
-mod probe;
-mod sock4;
-mod sock6;
-mod state;
